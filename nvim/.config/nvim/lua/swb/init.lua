d500273c7beb2683e53b571488ca147e7821f62f
Scott Breyfogle <sbreyfogle@descript.com>
2024-02-01 15:22:12 -0800

    Disable plugin autoreload

--- nvim/.config/nvim/lua/swb/init.lua
+++ nvim/.config/nvim/lua/swb/init.lua
@@ -4,7 +4,8 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = ','
 require("lazy").setup("swb/plugins",
     {
-        root = vim.fn.stdpath 'data' .. '/lazy_packages'
+        root = vim.fn.stdpath 'data' .. '/lazy_packages',
+        change_detection = { enabled = false },
     }
 )
 require("swb/keymaps")
