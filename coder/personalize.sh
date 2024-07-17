7c63e4de2f4687105d9f9e8e1533b92a73438681
sbreyfogle <sbreyfogle@descript.com>
2024-07-17 11:14:41 -0700

    More coder personalization

--- coder/personalize.sh
+++ coder/personalize.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # For coder workspaces: If only your home directory persists, what things need to be run at startup?
-#
+
+# Quick config
+sudo chsh scott -s /usr/bin/zsh
+sudo timedatectl set-timezone America/Los_Angeles
+
+# Reinstall neovim, and remove system fzf
 # Our default installation has older/incompatible versions of nvim and fzf
 sudo apt remove -y neovim fzf
 sudo tar -C /opt -xzf dotfiles/nvim-linux64.tar.gz
 
-sudo apt install -y ansible-lint
-sudo npm install -g prettier
-
-sudo chsh scott -s /usr/bin/zsh
+# Install other packages
+sudo apt install -y ansible-lint graphviz
+sudo npm install -g prettier typescript typescript-language-server
+pip install nvitop
