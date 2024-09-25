a2ebb1e13723bb0e4c812482367e00b0b6a102e4
Scott Breyfogle <sbreyfogle@descript.com>
2024-09-25 13:02:14 -0700

    Add Go language server

--- coder/personalize.sh
+++ coder/personalize.sh
@@ -13,4 +13,5 @@ sudo tar -C /opt -xzf dotfiles/nvim-linux64.tar.gz
 # Install other packages
 sudo apt install -y ansible-lint graphviz build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev libxi-dev libglu1-mesa-dev libglew-dev pkg-config
 sudo npm install -g prettier typescript typescript-language-server
+go install golang.org/x/tools/gopls@latest
 pip install nvitop
--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ git submodule update --recursive --init
 sudo apt install ansible-lint golang npm shellcheck 
 npm install -g prettier pyright typescript typescript-language-server
 pip install black flake8 reorder-python-imports
+go install golang.org/x/tools/gopls@latest
 go install github.com/rhysd/actionlint/cmd/actionlint@latest
 
 # stow needs to run from this directory
